91dd541c1cb602bae74cd94f5cbbf1d10fcfbf9d
mark <a0938765923@gmail.com>
2021-09-02 23:08:05 +0800

    logfail

--- SqliteDapper.Demo/Controllers/ProductController.cs
+++ SqliteDapper.Demo/Controllers/ProductController.cs
@@ -45,13 +45,23 @@ namespace SqliteDapper.Demo.Controllers
         [HttpGet]
         public async Task<IEnumerable<dynamic>> Get()
         {
+
+            //logger.LogInformation("Start : Getting item details for {ID}");
+
+            //List<string> list = new List<string>();
+
+            //list.Add("A");
+            //list.Add("B");
+
+            //logger.LogInformation($"Completed : Item details for  {{{string.Join(", ", list)}}}");
+
             //setConfig();
-            logger.LogTrace("Loggin Level = 0 (Trace)");
-            logger.LogDebug("Loggin Level = 1 (Debug)");
-            logger.LogInformation("Loggin Level = 2 (Information)");
-            logger.LogWarning("Loggin Level = 3 (Warning )");
-            logger.LogError("Loggin Level = 4 (Error)");
-            logger.LogCritical("Loggin Level = 5 (Critical)");
+            //logger.LogTrace("Loggin Level = 0 (Trace)");
+            //logger.LogDebug("Loggin Level = 1 (Debug)");
+            //logger.LogInformation("Loggin Level = 2 (Information)");
+            //logger.LogWarning("Loggin Level = 3 (Warning )");
+            //logger.LogError("Loggin Level = 4 (Error)");
+            //logger.LogCritical("Loggin Level = 5 (Critical)");
             return await productProvider.Get();
         }
 
